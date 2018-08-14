c0f57af5cc80f46d5e0ccc1b855da3760f255f15
alexHooper <alexmohack@gmail.com>
2018-08-15 00:51:26 +0530

    add view for our services page

--- website/views.py
+++ website/views.py
@@ -5,6 +5,10 @@ def index(request):
 	return render(request, 'index.html')
 
 
+def service_view(request):
+	return render(request, 'services.html')
+
+
 def send_view_visit(request):
 	view = request.GET.get("view")
 	data = {
