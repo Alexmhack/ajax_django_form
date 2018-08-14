d7d2085077f2c67730dbafd534cbc78791e82f5a
alexHooper <alexmohack@gmail.com>
2018-08-15 00:51:37 +0530

    add url for services page

--- website/urls.py
+++ website/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index-view')
+    path('services/', views.service_view, name='service-view'),
 ]
