0ec72522935c99c65ad1fd92bc93f40edd067b20
alexHooper <alexmohack@gmail.com>
2018-08-15 00:50:13 +0530

    add basic view for sending ajax request

--- website/views.py
+++ website/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def index(request):
 	return render(request, 'index.html')
+
+
+def send_view_visit(request):
+	view = request.GET.get("view")
+	data = {
+		'': '',
+	}
+	return JsonResponse(data)
